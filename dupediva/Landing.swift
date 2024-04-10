bd64d15f99893cceca94e881bdc1460861665138
pathiruk <pathiruk@ncsu.edu>
2024-04-09 23:12:04 -0400

    added camera, upload, and login stuff

--- dupediva/Landing.swift
+++ dupediva/Landing.swift
@@ -9,40 +9,116 @@ import Foundation
 import SwiftUI
 
 struct LandingView: View{
-    
     @State private var showSheet: Bool = false
+    @State var isPickerShowing = false
+    @State var selectedImage: UIImage?
+    
+    @State var isCamShowing = false
+    
     
     var body: some View{
         // Text("test class")
         NavigationStack{
-            
-            Button(
-                action:{
-                    performAPICall()
-                    showSheet.toggle()
-                },
-                label:{
-                    Text("API button")
+            VStack{
+                //selected image
+                if let image = selectedImage{
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 2)
+                                .stroke(Color.purple, lineWidth: 4))
+                        .frame(width:300, height:400)
+                    //api button
+                    Button(
+                        action:{
+                            performAPICall()
+                            showSheet.toggle()
+                        },
+                        label:{
+                            Text("Search for Similar Products(API button)")
+                        }
+                    ).buttonStyle(PurpleButtonStyle())
                 }
-            ).buttonStyle(.borderedProminent) .sheet(isPresented: $showSheet){
-                SheetView()
+               
+                
+                //upload and camera buttons
+                
+                HStack{
+                    Button{
+                        isPickerShowing = true
+                    }label:{
+                        Text("Select a Photo").foregroundColor(Color.white)
+                    }.buttonStyle(PurpleButtonStyle())
+                        .sheet(isPresented: $isPickerShowing, onDismiss: nil){
+                            //image picker
+                            ImagePicker(selectedImage: $selectedImage, isPickerShowing: $isPickerShowing)
+                        }
+                    //camera
+                    Button{
+                        isCamShowing = true
+                    }label:{
+                        Text("Take a Photo")
+                    }.buttonStyle(PurpleButtonStyle())
+                        .sheet(isPresented: $isCamShowing, onDismiss: nil){
+                            //image cam
+                            ImageCam(selectedImage: $selectedImage, isCamShowing: $isCamShowing)
+                        }
+                }.padding()
+                
+                //NavigationLink("Click here", destination: OtherView())
+                
+                //login button
+                Button(
+                    action:{
+                        
+                        print("test")
+                        showSheet.toggle()
+                    },
+                    label:{
+                        Text("Login")
+                    }
+                ).buttonStyle(PurpleButtonStyle()) .sheet(isPresented: $showSheet){
+                    SheetView(isSheetShowing: $showSheet)
+                }
             }
-            
-            NavigationLink("Click here", destination: OtherView())
         }
-    }}
+    }
+}
 
 
 struct SheetView: View{
     
-    @State var string:String = ""
+    @State var username:String = ""
+    @State var password:String = ""
+    @Binding var isSheetShowing: Bool
     
     var body: some View{
         NavigationView{
             ScrollView{
                 VStack{
-                    TextField("search for image", text: $string ).textFieldStyle(.roundedBorder)
-                }.padding()
+                    HStack{
+                        Text("Username:")
+                        TextField("Enter Username or Email", text: $username ).textFieldStyle(.roundedBorder)
+                    }.padding()
+                    HStack{
+                        Text("Password:")
+                        TextField("Enter Password", text: $password ).textFieldStyle(.roundedBorder)
+                    }.padding()
+                    
+                    Button(
+                        action:{
+                            
+                            print("test")
+                            isSheetShowing = false
+                        },
+                        label:{
+                            Text("Login")
+                        }
+                    ).buttonStyle(PurpleButtonStyle())
+                    
+                }
+                
             }
         }
     }
@@ -51,5 +127,17 @@ struct SheetView: View{
 struct TestView_Previews: PreviewProvider{
     static var previews: some View{
         LandingView()
+    }
+}
+
+
+
+struct PurpleButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(10)
+            .background(Color.purple)
+            .foregroundColor(.white)
+            .cornerRadius(5)
     }
 }
