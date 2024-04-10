babba590089ee259d2a713374e831b3478bc30e2
pathiruk <pathiruk@ncsu.edu>
2024-04-09 23:11:38 -0400

    added in camera/upload stuff and login button

---
+++ dupediva/ImageCam.swift
@@ -0,0 +1,8 @@
+//
+//  ImageCam.swift
+//  dupediva
+//
+//  Created by Preeti Thirukonda on 4/9/24.
+//
+
+import Foundation
---
+++ dupediva/ImagePicker.swift
@@ -0,0 +1,8 @@
+//
+//  ImagePicker.swift
+//  dupediva
+//
+//  Created by Preeti Thirukonda on 4/9/24.
+//
+
+import Foundation
