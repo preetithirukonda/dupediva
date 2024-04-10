1dd19b0e368cd8f039235cbed964103b15c9d406
pathiruk <pathiruk@ncsu.edu>
2024-04-09 22:32:08 -0400

    random changes, stuff doesnt work

--- dupediva/test.swift
+++ dupediva/test.swift
@@ -7,28 +7,79 @@
 
 import Foundation
 import SwiftUI
+import UIKit
 
 struct TestView: View{
     
     @State private var showSheet: Bool = false
+    @State var isPickerShowing = false
+    @State var selectedImage: UIImage?
     
+    @State var isCamShowing = false
+    @State var selectedImageCam: UIImage?
+   
+    
+  
     var body: some View{
         // Text("test class")
         NavigationStack{
-            
-            Button(
-                action:{
-                    print("test")
-                    showSheet.toggle()
-                },
-                label:{
-                    Text("test button")
+            VStack{
+                if let image = selectedImage{
+                    Image(uiImage: image).resizable()
+                        .frame(width:200, height:200)
                 }
-            ).buttonStyle(.borderedProminent) .sheet(isPresented: $showSheet){
-                SheetView()
-            }
+                
+               // if selectedImage != nil{
+               //     Image(uiImage: selectedImage!).resizable()
+               //         .frame(width:200, height:200)
+               // }
+                if selectedImageCam != nil{
+                    Image(uiImage: selectedImageCam!).resizable()
+                        .frame(width:200, height:200)
+                }
+                
+               
+                //picker
+                Button{
+                    isPickerShowing = true
+                }label:{
+                    Text("select a photo")
+                }.buttonStyle(.borderedProminent)
+                    .sheet(isPresented: $isPickerShowing, onDismiss: nil){
+                        //image picker
+                        ImagePicker(selectedImage: $selectedImage, isPickerShowing: $isPickerShowing)
+                    }
+                //camera
+                Button{
+                    isCamShowing = true
+                }label:{
+                    Text("take a photo")
+                }.buttonStyle(.borderedProminent)
+                    .sheet(isPresented: $isCamShowing, onDismiss: nil){
+                        //image cam
+                        ImageCam(selectedImageCam: $selectedImageCam, isCamShowing: $isCamShowing)
+                    }
+                
+                NavigationLink("Click here", destination: OtherView())
+                //test button
+                Button(
+                    action:{
+                        
+                        print("test")
+                        showSheet.toggle()
+                    },
+                    label:{
+                        Text("test button")
+                    }
+                ).buttonStyle(.borderedProminent) .sheet(isPresented: $showSheet){
+                    SheetView()
+                }
+                
+                
+              }
+          
             
-            NavigationLink("Click here", destination: OtherView())
+            
         }
     }}
 
